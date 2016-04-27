0957093339995ff0612be5aafb5f80798f696588
Jarrett Revels <jarrettrevels@gmail.com>
2016-04-27 13:36:59 -0400

    rm extra ENV usage

--- bin/setup_test_ci.jl
+++ bin/setup_test_ci.jl
@@ -5,9 +5,8 @@ import Nanosoldier, GitHub
 cpus = [1,2,3]
 auth = GitHub.authenticate(ENV["GITHUB_AUTH"])
 secret = ENV["GITHUB_SECRET"]
-user = ENV["SERVER_USER"]
 
-config = Nanosoldier.Config(user, nodes, cpus, auth, secret;
+config = Nanosoldier.Config(ENV["USER"], nodes, cpus, auth, secret;
                             workdir = joinpath(homedir(), "workdir"),
                             trackrepo = "jrevels/julia",
                             reportrepo = "jrevels/BaseBenchmarkReports",
