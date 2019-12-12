34c2856ad150d39de99c2afac516f310310ed9a6
Tim Besard <tim.besard@gmail.com>
2019-12-12 21:39:17 +0100

    Fix link to previous daily's log.

--- src/jobs/PkgEvalJob.jl
+++ src/jobs/PkgEvalJob.jl
@@ -501,7 +501,7 @@ function printreport(io::IO, job::PkgEvalJob, results)
                 if test.source == "both"
                     against_log = "logs/$(test.name_1)/$(test.julia_1).log"
                     if hasprevdate
-                        against_log = "../$(datedirname(results["previous_date"]))/$against_log"
+                        against_log = "../pkgeval-$(datedirname(results["previous_date"]))/$against_log"
                     end
                     print(io, " vs. [$(test.name_1)$(verstr(test.version_1))]($against_log)")
 
